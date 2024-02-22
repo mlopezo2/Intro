6d55be52171edd1a6b577094de0c3ad6883e19ca
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-02-22 10:19:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ image = Image.open("babyyoda.jpg")
 st.image(image, caption="Baby Yoda")
 
 texto = st.text_input("Capítulo 2, Temporada 1", "Baby Yoda, The Mandalorian")
-st.write("En la primera temporada descubrimos que el nombre real de Baby Yoda es Grogu. Pertenece a la misma especie del icónico maestro Yoda, de quien no se ha podido definiren la mitología del mundo cual es el nombre de su especie o de qué planeta proviene.")
+st.write("En la primera temporada descubrimos que el nombre real de Baby Yoda es Grogu."
+"Pertenece a la misma especie del icónico maestro Yoda, de quien no se ha podido definiren la mitología del mundo cual es el nombre de su especie o de qué planeta proviene.")
+
