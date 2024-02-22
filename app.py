a5c2eb6d3edc298cc70344fb48cb843a676dfb5a
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-02-22 10:11:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 from PIL import Image
 
 st.title("Primer intento")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open("babyyoda.jpg")
 
+st.image(image, caption="Baby Yoda")
+
+texto = st.text_input("Baby Yoda, The Mandalorian", "Capítulo 2, Temporada 1")
